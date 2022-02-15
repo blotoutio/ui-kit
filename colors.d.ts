aeb9c8a0678d5f8696cd18e34970f79a540a2ed5
NejcZdovc <nejc.zdovc@3zsistemi.si>
2022-02-15 17:22:21 +0100

    Adds 3 icons

---
+++ colors.d.ts
@@ -0,0 +1,65 @@
+export declare const black: string
+export declare const black80: string
+export declare const black64: string
+export declare const black48: string
+export declare const black32: string
+export declare const black24: string
+export declare const black16: string
+export declare const black8: string
+
+export declare const white: string
+
+export declare const brand: string
+export declare const brandHover: string
+export declare const brandActive: string
+
+export declare const errorEasy: string
+export declare const errorJuice: string
+
+export declare const attentionEasy: string
+export declare const attentionJuice: string
+
+export declare const successEasy: string
+export declare const successJuice: string
+
+export declare const link: string
+export declare const linkHover: string
+
+export declare const submarineEasy: string
+export declare const submarineJuice: string
+
+export declare const orange10: string
+export declare const orange20: string
+export declare const orange30: string
+export declare const orange40: string
+export declare const orange50: string
+export declare const orange60: string
+export declare const orange70: string
+export declare const orange80: string
+export declare const orange90: string
+export declare const orange100: string
+
+export declare const neutrals10: string
+export declare const neutrals20: string
+export declare const neutrals30: string
+export declare const neutrals40: string
+export declare const neutrals50: string
+export declare const neutrals60: string
+export declare const neutrals70: string
+export declare const neutrals80: string
+export declare const neutrals90: string
+export declare const neutrals100: string
+export declare const neutralsDark: string
+
+export declare const blue10: string
+export declare const blue20: string
+export declare const blue30: string
+export declare const blue40: string
+export declare const blue50: string
+export declare const blue60: string
+export declare const blue70: string
+export declare const blue80: string
+export declare const blue90: string
+export declare const blue100: string
+
+export declare const chartColors: string[]
