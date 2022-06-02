8077781cf659b50764b379be2f629660d98e0744
Gregor Hann <ihumhann@gmail.com>
2022-06-02 14:07:39 +0200

    Adjust handleKeyDown props

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,4 +1,8 @@
-import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react'
+import {
+  InputHTMLAttributes,
+  KeyboardEvent,
+  TextareaHTMLAttributes,
+} from 'react'
 import { EChartsOption } from 'echarts'
 
 export interface SelectValue {
@@ -190,8 +194,8 @@ interface LoaderProps {
 interface MultiSelectInputProps {
   handleChange: (value: SelectValue[]) => void
   handleInputChange: (inputValue: string) => void
-  handleKeyDown: (value: KeyboardEvent) => void
   inputValue: string | number
+  handleKeyDown: (event: KeyboardEvent) => void
   value: SelectValue[]
   className?: string
   error?: boolean
