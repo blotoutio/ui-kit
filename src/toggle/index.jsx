c9caf5a39f159dfd7f2616a61f0cdbd619b91581
Janez Stupar <janez@janezstupar.com>
2022-06-30 11:15:55 +0200

    Add toggle knob variant. Fix sidebar dialog margins.

--- src/toggle/index.jsx
+++ src/toggle/index.jsx
@@ -1,23 +1,44 @@
-import { Wrapper, Option } from './style'
+import { Wrapper, Option, ToggleButton } from './style'
 
-const Toggle = ({ selected, options, handleChange, isDisable, className }) => {
-  if (!selected) {
+const Toggle = ({
+  selected,
+  options,
+  handleChange,
+  isDisable,
+  className,
+  style = 'options',
+}) => {
+  if (selected === undefined) {
     console.error('Selected for toggle was not provided')
     return null
   }
 
+  const toggleClass =
+    style === 'toggle' ? (selected === true ? 'toggle active' : 'toggle') : null
+  const classNames = [className, style === 'toggle' ? toggleClass : null]
+    .join(' ')
+    .trim()
+
   return (
-    <Wrapper isDisable={isDisable} className={className}>
-      {options.map((option) => (
-        <Option
-          key={option.value}
-          onClick={isDisable ? undefined : handleChange.bind(this, option)}
-          active={selected.value === option.value}
+    <Wrapper isDisable={isDisable} className={classNames}>
+      {style === 'options' &&
+        options.map((option) => (
+          <Option
+            key={option.value}
+            onClick={isDisable ? undefined : handleChange.bind(this, option)}
+            active={selected.value === option.value}
+            isDisable={isDisable}
+          >
+            {option.label}
+          </Option>
+        ))}
+      {style === 'toggle' && (
+        <ToggleButton
+          className={selected ? 'active' : 'inactive'}
+          onClick={isDisable ? undefined : handleChange.bind(this, !selected)}
           isDisable={isDisable}
-        >
-          {option.label}
-        </Option>
-      ))}
+        />
+      )}
     </Wrapper>
   )
 }
